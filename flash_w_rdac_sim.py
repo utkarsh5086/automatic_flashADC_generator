a77f2192549ee54f5b15b3d94854e5de3c420f4b
utkarsh5086 <150561944+utkarsh5086@users.noreply.github.com>
2025-04-17 20:43:44 -0700

    Update flash_w_rdac_sim.py

--- flash_w_rdac_sim.py
+++ flash_w_rdac_sim.py
@@ -4,4 +4,4 @@ import subprocess
 
 #Popen(["ngspice","-r","rawfile.raw","-b","/Users/utkarshsharma/sscs_2024/strongARM_us/lvs/flash_w_rdac_eval_plotting.sp"],cwd="simulation").wait()
 
-Popen(["ngspice","-r","rawfile.raw","-b","/Users/utkarshsharma/sscs_2024/strongARM_us/lvs/flash_w_rdac_eval_3b.sp"],cwd="simulation").wait()
+Popen(["ngspice","-r","rawfile.raw","-b","/simulation/flash_w_rdac_eval_3b.sp"],cwd="simulation").wait()
