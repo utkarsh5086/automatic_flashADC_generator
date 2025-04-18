6d683129b545f01de5499213d572264a83a55358
utkarsh5086 <150561944+utkarsh5086@users.noreply.github.com>
2025-04-17 20:42:56 -0700

    Add files via upload

---
+++ flash_w_rdac_sim.py
@@ -0,0 +1,7 @@
+from subprocess import Popen,run
+import subprocess
+
+
+#Popen(["ngspice","-r","rawfile.raw","-b","/Users/utkarshsharma/sscs_2024/strongARM_us/lvs/flash_w_rdac_eval_plotting.sp"],cwd="simulation").wait()
+
+Popen(["ngspice","-r","rawfile.raw","-b","/Users/utkarshsharma/sscs_2024/strongARM_us/lvs/flash_w_rdac_eval_3b.sp"],cwd="simulation").wait()
